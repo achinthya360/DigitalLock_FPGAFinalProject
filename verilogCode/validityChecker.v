9608fb41bbbe914336bc94c56cfa2619ffe15f84
Achinthya Poduval <achinthya360@yahoo.com>
2020-06-09 02:09:59 -0700

    wrote out validity checker module

---
+++ verilogCode/validityChecker.v
@@ -0,0 +1,91 @@
+
+module validChecker (
+  input hwclk,
+  input wire bstate,
+  input wire [3:0]button,
+  input wire inputWrong,
+  input wire readInput,
+  input wire compareType[1:0],
+  input wire correctUC[23:0],
+  input wire correctPC[23:0],
+  input wire store,
+  output reg correct,
+  output wire newUC[23:0],
+
+  //output led1, output led2,output led3, output led4,
+);
+
+	wire [3:0] prev1, prev2, prev3, prev4, prev5, prev6;
+	reg [3:0] prevUC1, prevUC2, prevUC3, prevUC4, prevUC5, prevUC6;
+	
+	always @(negedge bstate) begin
+		if(readInput) begin // only update registers when inputs should be read
+			if((button[3:0]==8)||(button[3:0]==9)||(button[3:0]==7)) begin
+				prev1 = 0;
+				prev2 = 0;
+				prev3 = 0;
+				prev4 = 0;
+				prev5 = 0;
+				prev6 = 0;
+			end 
+			else begin
+				prev1 = prev2;
+				prev2 = prev3;
+				prev3 = prev4;
+				prev4 = prev5;
+				prev5 = prev6;
+				prev6 = button;
+			end 
+		end
+
+		if(compareType==2'b00) begin // compare PCs
+			if(prev1[3:0]==correctPC[23:20]
+				&& prev2[3:0]==correctPC[19:16]
+				&& prev3[3:0]==correctPC[15:12]
+				&& prev4[3:0]==correctPC[11:8]
+				&& prev5[3:0]==correctPC[7:4]
+				&& prev6[3:0]==correctPC[3:0]) begin
+				correct <= 1;
+			end
+		end
+		else if(compareType==2'b01) begin // compare UC to correct UC
+			if((prev1[3:0]==correctUC[23:20] | correctUC[23:20]==0)
+				&& (prev2[3:0]==correctUC[19:16] | correctUC[19:16]==0)
+				&& prev3[3:0]==correctUC[15:12]
+				&& prev4[3:0]==correctUC[11:8]
+				&& prev5[3:0]==correctUC[7:4]
+				&& prev6[3:0]==correctUC[3:0]) begin
+				correct <= 1;
+			end
+		end
+		else if(compareType==2'b10) begin// compare UC to previously entered input UC
+			if((prev1[3:0]==prevUC1[3:0] | prevUC1[23:20]==0)
+				&& (prev2[3:0]==prevUC2[3:0] | prevUC1[19:16]==0)
+				&& prev3[3:0]==prevUC3[3:0]
+				&& prev4[3:0]==prevUC4[3:0]
+				&& prev5[3:0]==prevUC5[3:0]
+				&& prev6[3:0]==prevUC6[3:0]) begin
+				correct <= 1;
+			end
+		end
+		else // store a UC for future comparison
+			prevUC1 = prev1;
+			prevUC2 = prev2;
+			prevUC3 = prev3;
+			prevUC4 = prev4;
+			prevUC5 = prev5;
+			prevUC6 = prev6;
+		end
+	end
+
+	always @(posedge store) begin
+		newUC[23:20] = prev1;
+		newUC[19:16] = prev2;
+		newUC[15:12] = prev3;
+		newUC[11:8] = prev4;
+		newUC[7:4] = prev5;
+		newUC[3:0] = prev6;
+	end
+
+
+endmodule
